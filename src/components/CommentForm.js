9d34952bf4d63cf83f25a0e7fc78fb88e0ae0382
Javier Granados <javier.granados@jitrixsoftware.com>
2017-07-27 17:38:15 +0200

    Comment form added

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -3,19 +3,17 @@ import React, { Component } from 'react'
 class CommentForm extends Component {
   submit = e => {
     e.preventDefault();
-    // TODO: aquí necesitas 2 variables...
-    // 1) el input con el autor del comentario
-    // 2) el input con el texto del comentario
+    const author = this.author.value
+    const comment = this.comment.value
     const { postId } = this.props.params
-    this.props.addComment(postId, /* TODO: author, comment */)
-    // TODO: no te olvides de resetear el formulario cuando acabes! 👆🏼
+    this.props.addComment(postId, author, comment)
+    this.form.reset()
   }
 
   render = () => (
-    // TODO: añade el evento `submit` y las `refs` que necesites!
-    <form className='comment-form'>
-      <input type='text' placeholder='author' />
-      <input type='text' placeholder='comment' />
+    <form className='comment-form' onSubmit={this.submit} ref={form => this.form = form}>
+      <input type='text' placeholder='author' ref={input => this.author = input} />
+      <input type='text' placeholder='comment' ref={input => this.comment = input}/>
       <input type='submit' hidden />
     </form>
   )
